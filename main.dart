63008fe236a52a10adfc52efced16d13c9a115a4
Ahmed Allam <ahmed.allam@izam.com>
2025-04-22 21:45:36 +0200

    List , Map

--- main.dart
+++ main.dart
@@ -1,6 +1,26 @@
 import 'dart:ffi';
 
 void main() {
-  /// DataType inputName = value
-  /// Map
+  Map<String, dynamic> myMap = {"id": 1, "name": "Ahmed", "dicount": true};
+  List<String> myList = ["Ahmed", "Alaa"];
+
+  List<Map<String, dynamic>> myData = [
+    {"id": 111, "name": "Product1"},
+    {"id": 122, "name": "Product2"},
+    {"id": 133, "name": "Product3"},
+    {"id": 144, "name": "Product4"},
+  ];
+
+  print(myList);
+  // print(myMap["name"]);
+
+  // myData.forEach((element) {
+  //   print(element["name"]);
+  // });
 }
+
+/*
+1- List<> int, string , bool
+2- Map ( key(string) , value () )
+3- List<Map> 
+*/
