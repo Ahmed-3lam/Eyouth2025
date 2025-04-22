959de1235ff1e36ea7770f6c9d5ba44e08c80325
Ahmed Allam <ahmed.allam@izam.com>
2025-04-22 20:30:42 +0200

    remove list

--- main.dart
+++ main.dart
@@ -2,17 +2,5 @@ import 'dart:ffi';
 
 void main() {
   /// DataType inputName = value
-
-  /// List
-  List<String> myList = ["Ahmed", "Mohamed", "Alaa"];
-  List<int> myList2 = [1, 2, 3, 4];
-  List<bool> myList3 = [true, false, true, false];
- 
-  print(myList);
-
-  // myList.forEach((element) {
-  //   print(element);
-  // });
-
   /// Map
 }
