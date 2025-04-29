2a3e147f191c6bb98cc766e9a35e70fd33c1326e
Ahmed Allam <ahmed.allam@izam.com>
2025-04-29 22:42:24 +0300

    Day 4: Functions

--- main.dart
+++ main.dart
@@ -1,17 +1,37 @@
 import 'dart:ffi';
 
 void main() {
-  /// While
+  printMyName("Ahmed", 22);
+  printMyNameWithNamedParams(name: "Ahmed");
 
-  int input = 10;
+  printMyName2("Ahmed");
+  printMyNameWithNamedParams2(name: "Ahmed");
 
-  while (input < 10) {
-    print(input);
-      input++;
-  }
 
-  do {
-    print(input);
-    input++;
-  } while (input < 10);
+}
+
+// Required Params - Optional(Named) Params
+///(1) Void
+
+//(i)UnNamed Param
+void printMyName(String name, int age) {
+  print(name);
+}
+
+//(ii) (Named Param)
+void printMyNameWithNamedParams({required String name, int age = 10}) {
+  print(name);
+}
+
+///---------------------------------------
+///(2) Return
+//(i)UnNamed Param
+
+String printMyName2(String name) {
+  return name;
+}
+
+//(ii) (Named Param)
+String printMyNameWithNamedParams2({required String name}) {
+  return name;
 }
---
+++ main_second.dart
@@ -0,0 +1,5 @@
+void main(){
+   print("My Name");
+  print("is");
+  print("Ahmed");
+}
